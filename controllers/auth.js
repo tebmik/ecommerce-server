2b2bdb1be9a06fe6160bc35c84aef038e4e0615a
tebmik <lloyd.s.tom@gmail.com>
2020-09-22 14:36:19 +0100

    changed err to error for front-end readability

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,24 @@
 const User = require("../models/user");
-require('dotenv').config();
+require("dotenv").config();
 const jwt = require("jsonwebtoken"); // to generate signed token
 const expressJwt = require("express-jwt"); // auth check
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
-    console.log("req-body", req.body)
+    console.log("req-body", req.body);
     const user = new User(req.body);
     user.save((err, user) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
-                err: errorHandler(err)
-            })
+                error: errorHandler(err),
+            });
         }
         user.salt = undefined;
         user.hashed_password = undefined;
         res.json({
-            user
-        })
-    })
+            user,
+        });
+    });
 };
 
 exports.signin = (req, res) => {
@@ -28,20 +28,23 @@ exports.signin = (req, res) => {
         // if err or no user
         if (err || !user) {
             return res.status(401).json({
-                error: 'User with that email does not exist. Please signup.'
+                error: "User with that email does not exist. Please signup.",
             });
         }
         // if user is found make sure the email and password match
         // create authenticate method in model and use here
         if (!user.authenticate(password)) {
             return res.status(401).json({
-                error: 'Email and password do not match'
+                error: "Email and password do not match",
             });
         }
         // generate a token with user id and secret
-        const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET);
+        const token = jwt.sign(
+            { _id: user._id, role: user.role },
+            process.env.JWT_SECRET
+        );
         // persist the token as 't' in cookie with expiry date
-        res.cookie('t', token, { expire: new Date() + 9999 });
+        res.cookie("t", token, { expire: new Date() + 9999 });
         // retrun response with user and token to frontend client
         const { _id, name, email, role } = user;
         return res.json({ token, user: { _id, email, name, role } });
@@ -60,34 +63,21 @@ exports.requireSignin = expressJwt({
 });
 
 exports.isAuth = (req, res, next) => {
-    let user = 
-            req.profile && 
-            req.auth && 
-            req.profile._id == req.auth._id;
-    
-    if(!user) {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+
+    if (!user) {
         return res.status(403).json({
-            error: "Access denied."
+            error: "Access denied.",
         });
     }
     next();
 };
 
 exports.isAdmin = (req, res, next) => {
-    if(req.profile.role === 0) {
+    if (req.profile.role === 0) {
         return res.status(403).json({
-            error: "Admin resource! Access denied."
+            error: "Admin resource! Access denied.",
         });
     }
     next();
 };
-
-
-
-
-
-
-
-
-
-
--- controllers/category.js
+++ controllers/category.js
@@ -3,9 +3,9 @@ const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.categoryById = (req, res, next, id) => {
     Category.findById(id).exec((err, category) => {
-        if(err || !category) {
+        if (err || !category) {
             return res.status(400).json({
-                error: "Category does not exist."
+                error: "Category does not exist.",
             });
         }
         req.category = category;
@@ -15,15 +15,15 @@ exports.categoryById = (req, res, next, id) => {
 
 exports.create = (req, res) => {
     const category = new Category(req.body);
-    
+
     category.save((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
-                error: errorHandler(err)
-            })
+                error: errorHandler(error),
+            });
         }
         res.json({ data });
-    })
+    });
 };
 
 exports.read = (req, res) => {
@@ -33,12 +33,12 @@ exports.read = (req, res) => {
 exports.update = (req, res) => {
     let category = req.category;
     category.name = req.body.name;
-    
+
     category.save((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
-                error: errorHandler(err)
-            })
+                error: errorHandler(err),
+            });
         }
         res.json(data);
     });
@@ -46,9 +46,9 @@ exports.update = (req, res) => {
 
 exports.list = (req, res) => {
     Category.find().exec((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
-                error: errorHandler(err)
+                error: errorHandler(err),
             });
         }
         res.json(data);
@@ -58,14 +58,14 @@ exports.list = (req, res) => {
 exports.remove = (req, res) => {
     let category = req.category;
     category.remove((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
-                error: errorHandler(err)
+                error: errorHandler(err),
             });
         }
         // res.json(data);
         res.json({
-            message: "Category successfully deleted."
+            message: "Category successfully deleted.",
         });
     });
 };
